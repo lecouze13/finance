414f42950f6757ec99057c6705433f1b1783c496
lecouze13 <lorenzo.geano@etu.univ-amu.fr>
2024-11-25 08:23:43 +0100

    ajout de title et fix de emprunt

--- finance/src/app/taux-emprunt/taux-emprunt.component.ts
+++ finance/src/app/taux-emprunt/taux-emprunt.component.ts
@@ -20,10 +20,12 @@ export class TauxEmpruntComponent {
   interet: number = 3.6;
   isCalculCoutTotal: boolean = false;
   isCalculMensualite: boolean = false;
+  isCapaciteEmprunt: boolean = false;
   dureeDuCredit: number |undefined = 0;
   coutTotal: number | undefined = 0;
   mensualite: number | undefined = 0;
-  calcul: String[] = ['Calculer le coût total', 'Calculer la mensualité', ];
+  capaciteEmprunt: number | undefined = 0;
+  calcul: String[] = ['Calculer le coût total', 'Calculer la mensualité', 'Calculer la capacité d\'emprunt'];
   selectedCalcul: String | undefined;
   coutCredit: Number | undefined;    
 
@@ -47,13 +49,28 @@ export class TauxEmpruntComponent {
       this.mensualite = (this.sommeEmprunte * tauxMensuel) / (1 - Math.pow(1 + tauxMensuel, -nombreDeMois));
     }
   }
+  calculCapaciteEmprunt(){
+    if(this.mensualite && this.interet && this.dureeDuCredit){
+      const tauxMensuel = this.interet / 100 / 12;
+      const nombreDeMois = this.dureeDuCredit;
+      this.capaciteEmprunt = Math.round(this.mensualite / (tauxMensuel / (1 - Math.pow(1 + tauxMensuel, -nombreDeMois))))
+    }
+  }
   changeCalcul(){
     if(this.selectedCalcul === 'Calculer le coût total'){
       this.isCalculMensualite=false
       this.isCalculCoutTotal=true
+      this.isCapaciteEmprunt=false
+
     }else if(this.selectedCalcul === 'Calculer la mensualité'){
       this.isCalculMensualite=true
       this.isCalculCoutTotal=false
+      this.isCapaciteEmprunt=false
+    }
+    else if (this.selectedCalcul === 'Calculer la capacité d\'emprunt'){
+      this.isCalculMensualite=false
+      this.isCalculCoutTotal=false
+      this.isCapaciteEmprunt=true
     }
   }
 }
